6c1ac80240b8bbd4c39f34d13ba8dbc222170648
Danny Marks <dannywmarks@gmail.com>
2021-08-06 08:08:57 -0700

    cryptohash function added to mineBlock function

--- block.test.js
+++ block.test.js
@@ -1,5 +1,6 @@
 const Block = require("./block");
 const { GENESIS_DATA } = require("./config");
+const cryptoHash = require('./crypto-hash')
 
 describe("Block", () => {
   const timestamp = "a-date";
@@ -37,20 +38,23 @@ describe("Block", () => {
     const data = "mined data";
     const minedBlock = Block.mineBlock({ lastBlock, data });
 
-    it('returns a Block instance', () => {
-      expect(minedBlock instanceof Block).toBe(true)
-    })
+    it("returns a Block instance", () => {
+      expect(minedBlock instanceof Block).toBe(true);
+    });
 
-    it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+    it("sets the `lastHash` to be the `hash` of the lastBlock", () => {
       expect(minedBlock.lastHash).toEqual(lastBlock.hash);
-    })
+    });
 
-    it('sets the `data`', () => {
+    it("sets the `data`", () => {
       expect(minedBlock.data).toEqual(data);
-    })
+    });
 
-    it('sets a `timestamp', () => {
-      expect(minedBlock.timestamp).not.toEqual(undefined)
-    })
+    it("sets a `timestamp", () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
+    it("creates a sha256 hash based on proper inputs", () => {
+      expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+    });
   });
 });
