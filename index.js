03aac97142a23fcd4ccd6be2403c6dfd58987fcc
Danny Marks <dannywmarks@gmail.com>
2021-08-15 19:48:52 -0700

    Optimized the implementation to avoid redundant interaction.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const request = require("request");
+const axios = require("axios");
 const Blockchain = require("./blockchain");
 const PubSub = require("./pubsub");
 
@@ -6,24 +9,54 @@ const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
+const DEFAULT_PORT = 3001;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
+app.use(bodyParser.json());
 
-app.use(express.json());
+// setTimeout(() => pubsub.broadcastChain(), 1000);
 
-setTimeout(() => pubsub.broadcastChain(), 1000);
-
-app.get("/api/blocks", (req, res) => {
+app.get("/api/blocks", async (req, res) => {
   res.json(blockchain.chain);
 });
 
 app.post("/api/mine", (req, res) => {
   const { data } = req.body;
+
   blockchain.addBlock({ data });
+
+  pubsub.broadcastChain();
 
   res.redirect("/api/blocks");
 });
 
-const PORT = 3000;
+// make request to root node APIs endpoint
+const syncChains = () => {
+  request(
+    { url: `${ROOT_NODE_ADDRESS}/api/blocks` },
+    (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = body;
+        console.log(rootChain);
+
+        console.log("replace chain on a sync with", rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    }
+  );
+};
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
+
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
 });
